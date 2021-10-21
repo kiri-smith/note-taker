cee94e730d20121a9032b59fe8cea7d0367fe36a
Kiri <kirilynne@gmail.com>
2021-10-21 11:05:04 -0400

    add app.get

--- server.js
+++ server.js
@@ -11,3 +11,12 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+})
+
+app.get("/api/notes", async (req, res) => {
+    const notes = await fs.readFile(path.join(__dirname, "db", "db.json"), { encoding: "utf-8" });
+    const notesJSON = JSON.parse(notes);
+    res.json(notesJSON);
+});
